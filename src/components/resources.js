9a4b614f2b4e46a7721b6777c91e850d204e019a
Hansmu <hans.miikael@gmail.com>
2016-07-18 13:49:37 +0300

    Added header and routes.

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+    return(
+        <div>
+            Super special recipe
+            <ul>
+                <li>Banana</li>
+                <li>Oil</li>
+                <li>Uranium</li>
+            </ul>
+        </div>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/app';
+import Resources from './components/resources';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <Route path="resources" component={Resources} />
+        </Route>
+    </Router>
   </Provider>
   , document.querySelector('.container'));
+
+//Provider wraps our Redux Store, the library that holds our actual state. Watches the Redux store directly. Tell the Provider whenever the state changes.
+//Provider broadcasts to every connected component that there has been a change, do whatever you want with it.
+//Connect() is a higher order component that is making direct communication with the Provider. Provider is what has access to the store.
