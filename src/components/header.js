b520d1761f5c0536109141c49c065079cbc4b2e3
Hansmu <hans.miikael@gmail.com>
2016-07-18 14:09:07 +0300

    Sign in/out button.

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import * as actions from '../actions';
 
 class Header extends Component {
 
     authButton() {
-        return <button>Sign In</button>;
+        if (this.props.authenticated) {
+            return(
+                <button onClick={ () => { this.props.authenticate(false) }}>
+                    Sign Out
+                </button>
+            );
+
+        }
+
+        return(
+            <button onClick={() => { this.props.authenticate(true) }}>
+                Sign In
+            </button>
+        );
+
     }
 
     render() {
@@ -26,4 +42,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+}
+
+export default connect(mapStateToProps, actions)(Header);
